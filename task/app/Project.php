e3018a2d661be299d5c7d554da47a30f1735d4a0
MahmoudAH <mah199645@gmail.com>
2018-09-20 12:15:12 +0200

    Update Project.php

--- task/app/Project.php
+++ task/app/Project.php
@@ -9,4 +9,13 @@ class Project extends Model
      protected $fillable = [
         'title', 'image','country',
     ];
+      public function country()
+  {
+    return $this->belongsTo('App\Country');
+  }
+
+  public function subprojects()
+  {
+    return $this->hasMany('App\Subproject');
+  }
 }
