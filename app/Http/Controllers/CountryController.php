fb943f1091c4b1f77ac0a6aa7ce62195a48ac80c
NOGA\3kely <mah199645@gmail.com>
2018-10-04 17:17:56 +0200

    edilt

--- app/Http/Controllers/CountryController.php
+++ app/Http/Controllers/CountryController.php
@@ -17,45 +17,43 @@ class CountryController extends Controller
         $this->middleware('auth');
     }
     
+    //show country data from country table
     public function index()
     {
        $countries =Country::paginate(5);
 
         return view('admin.manage.countries.index',compact('countries'));
     }
+    //
       public function create()
     {
       $countries =Country::all();
       return view('admin.manage.countries.create',compact('countries'));
     }
 
-    ///show
+    //implement previous and next link
 
     public function show($id)
-{
+    {
 
     $country = Country::find($id);
-
     $previous = Country::where('id', '<', $country->id)->max('id');
-
     $next = Country::where('id', '>', $country->id)->min('id');
-   //$country = Country::find(5); 
-
     return View::make('country.show')->with('previous', $previous)->with('next', $next);
-}
+    }
 
     /**
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
-     */    
+     */ 
+     //store new country data in country table   
     public function store(Request $request)
     {
       $this->validate($request, [
         'title' => 'required|max:100',
          'image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
-
       ]);
 
       $country = new Country();
@@ -70,7 +68,8 @@ class CountryController extends Controller
      $country->save();
      return back()->with('message', 'Country created!');
   
-}
+    }
+   // edit country data
     public function edit($id)
     {
       $country = Country::findOrFail($id);
@@ -84,6 +83,8 @@ class CountryController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
     /**/
+
+     //update country data
     public function update(Request $request, $id)
     {
       $this->validate($request, [
@@ -91,32 +92,25 @@ class CountryController extends Controller
 
       ]);
 
-      
       $country = Country::findOrFail($id);
       $country->title = $request->title;
       if ($request->file('image')){
-
       $image = Input::file('image');
       $filename  = time() . '.' . $image->getClientOriginalName();
       $path = public_path('images/' . $filename);
       Image::make($image->getRealPath())->resize(100,100)->save($path);
        $country->update(['image'=>"/images/{$filename}"]);
-}
+    }
       $country->save();
-
       //Session::flash('success', 'Successfully update the '. $country->title . ' title in the database.');
       return redirect()->route('country.index', $id)->with('message', 'Successfully updated!');
     }
-    
+   
+   //delete country data 
     public function destroy($id)
     {
-     
       $country = Country::findOrFail($id);
-      //unlink(public_path(). $country->image->file);
-
       $country->delete();
       return back()->with('message', 'Successfully deleted!');
-
-
     }
 }
